e66091864cdd59beb5e51798025b982bbc2dee3a
acmaaddi <48102725+acmaaddi@users.noreply.github.com>
2019-03-22 15:19:29 -0400

    Add files via upload

---
+++ Midterm Coding Project/src/test/java/pkgShape/testShape.java
@@ -0,0 +1,28 @@
+package pkgShape;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import pkgShape.Rectangle;
+
+public class testShape {
+	
+	
+
+	@Test 
+	public void testRectangle(){
+		
+	
+		ArrayList<pkgShape.Rectangle> rectangles_list= new ArrayList<pkgShape.Rectangle>();
+		rectangles_list.add(Rectangle(5,10));
+		rectangles_list.add(Rectangle(9,11));
+		
+		
+		
+	}
+}
+	
+
